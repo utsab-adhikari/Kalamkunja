2b282c0ba2812f05406cc9e7664ea313c7c7ac6f
Utsab Adhikari <utsab1adhikari@gmail.com>
2025-08-12 10:39:10 +0545

    profiles added

---
+++ src/app/api/v1/authors/route.js
@@ -0,0 +1,35 @@
+import connectDB from "@/db/ConnectDB";
+import Article from "@/models/articleModel";
+import User from "@/models/userModel";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    await connectDB();
+
+    const users = await User.find({});
+
+    const authorsWithArticles = await Article.distinct("authorId");
+
+    const profiles = users.map((user) => ({
+      ...user.toObject(),
+      profile: authorsWithArticles.some(
+        (authorId) => authorId.toString() === user._id.toString()
+      ),
+    }));
+
+    return NextResponse.json({
+      status: 200,
+      success: true,
+      message: "Users loaded successfully",
+      users: profiles,
+    });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({
+      status: 500,
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+}
---
+++ src/app/api/v1/users/[id]/route.js
@@ -0,0 +1,40 @@
+import connectDB from "@/db/ConnectDB";
+import User from "@/models/userModel";
+import { NextResponse } from "next/server";
+import mongoose from "mongoose";
+
+export async function DELETE(_, { params }) {
+  const { id } = await params;
+
+  if (!mongoose.isValidObjectId(id)) {
+    return NextResponse.json({
+      status: 400,
+      success: false,
+      message: "Invalid user id",
+    });
+  }
+
+  try {
+    await connectDB();
+    const deleted = await User.findByIdAndDelete(id).lean();
+    if (!deleted) {
+      return NextResponse.json({
+        status: 404,
+        success: false,
+        message: "User not found",
+      });
+    }
+    return NextResponse.json({
+      status: 200,
+      success: trtue,
+      message: "User deleted",
+      data: deleted,
+    });
+  } catch (err) {
+    return NextResponse.json({
+      status: 500,
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+}
